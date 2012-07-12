436294a4b3f3b5690154453613fd3921fe66d9c7
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-07-12 15:58:30 -0500

    Deprecate er_date model

--- app/models/er_date.rb
+++ app/models/er_date.rb
@@ -1,16 +1,5 @@
-# == Schema Information
-#
-# Table name: er_dates
-#
-#  cap            :integer
-#  created_at     :datetime
-#  emails_sent_at :datetime
-#  id             :integer          not null, primary key
-#  open           :boolean
-#  session_time   :datetime
-#  updated_at     :datetime
-#
-
+# This class is deprecated. This is just being kept around
+# for legacy data
 class ErDate < ActiveRecord::Base
   
   validates :cap,           presence: true
