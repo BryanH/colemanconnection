ac31ef8e53881dbba22503d47fb137966bdef5ed
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-08-01 16:36:39 -0500

    Actually assert something with the sign_in request specs

--- spec/support/shared_examples_for_sign_in.rb
+++ spec/support/shared_examples_for_sign_in.rb
@@ -2,5 +2,6 @@ shared_examples "successful sign in" do
   it "should sign in successfully" do
     visit new_user_session_path
     submit_sign_in_form
+    ['/students', '/staff'].should include(page.current_path)
   end
 end
