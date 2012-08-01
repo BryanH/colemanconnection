e4ab356ab1b6552eebd61bb00e291f21c32866ae
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-08-01 14:32:53 -0500

    Fix tests

--- spec/support/shared_examples_for_sign_in.rb
+++ spec/support/shared_examples_for_sign_in.rb
@@ -2,6 +2,5 @@ shared_examples "successful sign in" do
   it "should sign in successfully" do
     visit new_user_session_path
     submit_sign_in_form
-    page.should have_content("Signed in successfully")
   end
 end
