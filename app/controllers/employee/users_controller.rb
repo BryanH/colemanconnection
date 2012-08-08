9634a7f8180b1fd6b35c80170f1568a9399e506c
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-08-08 14:22:24 -0500

    Add employee user edit/update actions

--- app/controllers/employee/users_controller.rb
+++ app/controllers/employee/users_controller.rb
@@ -5,4 +5,17 @@ class Employee::UsersController < Employee::BaseController
     @users = Staff.paginate(page: params[:page]).active(isactive).search(params[:q])
   end
   
+  def edit
+    @user = Staff.find_by_id(params[:id])
+  end
+  
+  def update
+    @user = Staff.find_by_id(params[:id])
+    if @user.update_attributes(params[:staff])
+      redirect_to edit_employee_user_path(@user), flash: { success: "Account details have been updated for #{@user.name}" }
+    else
+      render :edit
+    end
+  end
+  
 end
