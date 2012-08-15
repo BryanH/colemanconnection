34ec7b7f5c0eca3c2d9a7ce99b13eb8c512a2641
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-08-15 14:11:52 -0500

    Create show action for Employee::ProgramDates

--- app/controllers/employee/program_dates_controller.rb
+++ app/controllers/employee/program_dates_controller.rb
@@ -4,4 +4,9 @@ class Employee::ProgramDatesController < Employee::BaseController
     @dates = Hash[ProgramDate.all.group_by(&:program).sort]
   end
   
+  def show
+    @date = ProgramDate.find_by_id(params[:id])
+    @sessions = @date.sessions.search_on_user(params[:q]).includes(:user).order(user: :last_name).page(params[:page])
+  end
+  
 end
