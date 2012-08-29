7675be00f7dff47398d6d8202dbcbde4782071af
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-08-29 10:55:02 -0500

    Fix typos in capistrano recipes

--- config/deploy/recipes/postgresql.rb
+++ config/deploy/recipes/postgresql.rb
@@ -13,7 +13,7 @@ namespace :postgresql do
   
   desc "Symlink the database.yml file into latest release"
   task :symlink, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{current_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   before "deploy:assets:precompile", "postgresql:symlink"
 end
