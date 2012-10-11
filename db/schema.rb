5c76607f4bd28265357e05dec7a5457f22ff3909
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-10-11 14:45:16 -0500

    Add Chemical Dependency Certificate (LCDC) to program listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121010195737) do
+ActiveRecord::Schema.define(:version => 20121011193709) do
 
   create_table "audits", :force => true do |t|
     t.integer  "auditable_id",                   :null => false
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,60 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-class ProgramSessionTimes
-
-  # Program Session Dates
-  OCCURS_ON_TIMES = [
-    nil,                           # 0
-    "September 20th at noon",      # 1
-    "October 4th at 5:30pm",       # 2
-    "October 18th at noon",        # 3
-    "November 1st at 5:30pm",      # 4
-    "November 15th at noon",       # 5
-    "December 6th at 5:30pm",      # 6
-    nil,                           # 7
-    "January 3rd 2013 at 5:30pm",  # 8
-    "January 17th 2013 at noon",   # 9
-    "February 7th 2013 at 5:30pm", # 10
-    "February 21st 2013 at noon",  # 11
-    "March 7th 2013 at 5:30pm",    # 12
-    "March 21st 2013 at noon",     # 13
-    "April 4th 2013 at 5:30pm",    # 14
-    "April 18th 2013 at noon",     # 15
-    "May 2nd 2013 at 5:30pm",      # 16
-    "May 16th 2013 at noon",       # 17
-    "June 6th 2013 at 5:30pm",     # 18
-    "June 20th 2013 at noon",      # 19
-    nil,                           # 20
-    "July 18th 2013 at noon",      # 21
-    "August 1st 2013 at 5:30pm",   # 22
-    "August 15th 2013 at noon"     # 23
-  ]
-  
-  def self.all
-    OCCURS_ON_TIMES.compact
-  end
-  
-  def self.first_thursdays
-    OCCURS_ON_TIMES.in_groups_of(2).collect { |x| x[0] }.compact
-  end
-  
-  def self.third_thursdays
-     OCCURS_ON_TIMES.in_groups_of(2).collect { |x| x[1] }.compact
-  end
-  
-  def self.already_created?(time, program)
-    time = Chronic.parse(time)
-    ProgramDate.exists?(occurs_on: time, program: program)
-  end
-  
-end
-
 {
   "Biosafety" => :first_thursdays,
   "Certified Prevention Specialist" => :both,
   "Clinical Laboratory Technician" => :first_thursdays,
-  "Community Health Worker" => :both,
+  "Community Health Worker" => :first_thursdays,
   "Computed Tomography" => :both,
   "Dental Assisting" => :third_thursdays,
   "Dental Hygiene" => :third_thursdays,
@@ -68,7 +19,7 @@ end
   "Health Care Career Academy" => [2, 5, 6, 9, 10, 13, 14, 17, 18, 21, 22],
   "Health Information Technology" => :both,
   "Histology Technician" => :first_thursdays,
-  "Human Service Technology" => :both,
+  "Human Service Technology" => :first_thursdays,
   "Medical Assistant" => :first_thursdays,
   "Nuclear Medicine Technologist" => :both,
   "Nursing (Registered Nursing)" => :both,
@@ -79,7 +30,8 @@ end
   "Physical Therapist Assistant" => [2, 4, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 22],
   "Radiography" => :both,
   "Respiratory Therapist" => :first_thursdays,
-  "Surgical Technology" => [2, 5, 6, 9, 10, 13, 14, 17, 18, 21, 22]
+  "Surgical Technology" => [2, 5, 6, 9, 10, 13, 14, 17, 18, 21, 22],
+  "Chemical Dependency Certificate (LCDC)" => :first_thursdays
 }.each_pair do |program, schedule|
   if schedule.is_a?(Symbol)
     case schedule
