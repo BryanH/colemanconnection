bb2fd713af0be082e45e79fe04bceb8964c41da6
Terry Schmidt <terry.m.schmidt@gmail.com>
2013-01-28 16:13:59 -0600

    RBP: Fix Law of Demeter warning for SessionPresenter

--- spec/presenters/session_presenter_spec.rb
+++ spec/presenters/session_presenter_spec.rb
@@ -5,7 +5,8 @@ describe SessionPresenter do
   let(:presenter) { SessionPresenter.new(object, view) }
   subject { presenter }
   
-  it { should respond_to(:program) }
+  it { should respond_to(:program_date_program) }
+  it { should respond_to(:program_date_occurs_on) }
   it { should respond_to(:occurs_on) }
   it { should respond_to(:attended?) }
 end
