f0593fb66a79b7d93339e0baa4ea1f7a8e1a6dd1
Terry Schmidt <terry.m.schmidt@gmail.com>
2015-02-02 10:11:47 -0600

    Add printable checklist for program sessions

--- app/controllers/employee/program_sessions_controller.rb
+++ app/controllers/employee/program_sessions_controller.rb
@@ -1,27 +1,32 @@
 class Employee::ProgramSessionsController < Employee::BaseController
-  before_filter :set_program_date, only: [:show, :happiness]
+  before_filter :set_program_date, only: [:show, :happiness, :printable]
+  layout 'print', only: :printable
   
-    def index
-      program_ids = Program.where(name: current_user.affiliated_programs, active: true).select(:id).collect(&:id)
-      @program_dates ||= ProgramDate.all_with_sessions.where{ program_id.in(program_ids) & occurs_on.gte(2.months.ago) }.group_by {|p| p.program_name }.sort
-    end
+  def index
+    program_ids = Program.where(name: current_user.affiliated_programs, active: true).pluck(:id)
+    @program_dates ||= ProgramDate.all_with_sessions.where{ program_id.in(program_ids) & occurs_on.gte(2.months.ago) }.group_by {|p| p.program_name }.sort
+  end
+
+  def show
+    authorize! :view, ProgramDate
   
-    def show
-      authorize! :view, ProgramDate
-    
-      @sessions ||= @date.sessions.search_on_user(params[:q]).includes(:user).order(user: :last_name).page(params[:page])
-      @audits ||= (@date.attendance_audits + @date.audits).sort_by(&:created_at).reverse
-    end
+    @sessions ||= @date.sessions.search_on_user(params[:q]).includes(:user).order(user: :last_name).page(params[:page])
+    @audits ||= (@date.attendance_audits + @date.audits).sort_by(&:created_at).reverse
+  end
+
+  def happiness
+    authorize! :view, ProgramDate
   
-    def happiness
-      authorize! :view, ProgramDate
-    
-      @completed_survey_results ||= @date.survey_results.not_pending.order(:id)
-    end
+    @completed_survey_results ||= @date.survey_results.not_pending.order(:id)
+  end
+  
+  def printable
+    authorize! :view, ProgramDate
+  end
   
   private
 
-    def set_program_date    
-      @date ||= ProgramDate.find_by_id(params[:id])
-    end
+  def set_program_date    
+    @date ||= ProgramDate.find_by_id(params[:id])
+  end
 end
