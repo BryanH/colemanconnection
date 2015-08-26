30ea22ce4f5324ca96fedfbe09312c4f4ada6b2b
Bryan Hanks, PMP <bryan@master-developer.com>
2015-08-26 13:28:20 -0500

    Manage delayed job and use sudo

--- config/deploy/recipes/delayed_job.rb
+++ config/deploy/recipes/delayed_job.rb
@@ -1,6 +1,6 @@
 require 'delayed/recipes'
 
 # Not working so commented out for now
-#after "deploy:stop",    "delayed_job:stop"
-#after "deploy:start",   "delayed_job:start"
-#after "deploy:restart", "delayed_job:restart"
+after "deploy:stop",    "delayed_job:stop"
+after "deploy:start",   "delayed_job:start"
+after "deploy:restart", "delayed_job:restart"
