06fa99798bcf844e1ec98798c2b35d0af7c9c8b7
Bryan Hanks, PMP <bryan@master-developer.com>
2015-09-04 14:57:23 -0500

    Correct variable for release path capistrano

--- config/deploy/recipes/fix_permissions.rb
+++ config/deploy/recipes/fix_permissions.rb
@@ -2,7 +2,7 @@ namespace :deploy do
 		namespace :permissions do
 				desc "Fix file permissions so group is 'capdeploy'"
 				task :fix do
-						run_rootsh "chgrp -R capdeploy #{destination}"
+						run_rootsh "chgrp -R capdeploy #{release_path}"
 				end
 
 		end
