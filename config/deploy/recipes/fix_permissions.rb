f20b5ca855fc13955680f3189410534de90b591a
Bryan Hanks, PMP <bryan@master-developer.com>
2015-09-04 14:50:10 -0500

    Move permissions into deploy namespace

--- config/deploy/recipes/fix_permissions.rb
+++ config/deploy/recipes/fix_permissions.rb
@@ -1,8 +1,10 @@
-namespace :permissions do
-		desc "Fix file permissions so group is 'capdeploy'"
-		task :fix do
-				run_rootsh "chgrp -R capdeploy #{destination}"
-		end
+namespace :deploy do
+		namespace :permissions do
+				desc "Fix file permissions so group is 'capdeploy'"
+				task :fix do
+						run_rootsh "chgrp -R capdeploy #{destination}"
+				end
 
-		after "deploy:install", "permissions:fix"
+		end
+		after "deploy:install", "deploy:permissions:fix"
 end
